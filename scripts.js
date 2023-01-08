8430b5414d5b9727fc3e1d47715fa94c0ece6f4d
Yonathan Herrera <shelburneofficial@gmail.com>
2023-01-08 16:49:30 -0300

    0.0.4 - Se agregó un background-image con imagenes. - Se agregó una opacidad en el background-image. - Se modificó el diseño en general de la página web. - El proyecto pasó de llamarse "Shelburne Games" a "Shelburne Unseen".

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Script para el background-image de la página principal, creado para que cada vez que se refresque la página salga una imágen nueva.
+window.onload = function bgimage () {
+    var images = ['css/principalb1.png', 'css/principalb2.png', 'css/principalb3.png'];
+    var image = images[Math.floor(Math.random() * images.length)];
+    document.getElementsByTagName('body')[0].style.backgroundImage = "linear-gradient(rgba(15, 13, 13, 0.8), rgba(15, 13, 13, 0.4)), url('" + image + "')";
+    document.getElementsByTagName('body')[0].style.backgroundRepeat = "no-repeat";
+    document.getElementsByTagName('body')[0].style.backgroundSize = "cover";
+    document.getElementsByTagName('body')[0].style.backgroundAttachment= "fixed";
+}
