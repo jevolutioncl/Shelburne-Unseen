cd50f3b4a61216999666e87d697358cbaf9e4767
Yonathan Herrera <shelburneofficial@gmail.com>
2023-01-09 19:14:37 -0300

    0.0.5 - Se agregó la pestaña historia. - Preparación para que el sitio web sea adaptable a dispositivos móviles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 // Script para el background-image de la página principal, creado para que cada vez que se refresque la página salga una imágen nueva.
+
 window.onload = function bgimage () {
     var images = ['css/principalb1.png', 'css/principalb2.png', 'css/principalb3.png'];
     var image = images[Math.floor(Math.random() * images.length)];
@@ -6,4 +7,5 @@ window.onload = function bgimage () {
     document.getElementsByTagName('body')[0].style.backgroundRepeat = "no-repeat";
     document.getElementsByTagName('body')[0].style.backgroundSize = "cover";
     document.getElementsByTagName('body')[0].style.backgroundAttachment= "fixed";
+    document.getElementsByTagName('body')[0].style.backgroundPosition = "center";   
 }
